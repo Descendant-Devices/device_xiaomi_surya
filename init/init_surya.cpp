d11548e9d77ad89197bbb84408b7f57f0ba60b41
LuK1337 <priv.luk@gmail.com>
2020-08-12 11:29:08 +0000

    surya: init: Add 6 & 8 RAM support
    Change-Id: I466f1eccc964204dd51f1c044346ff2c707d6c37
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>
    Signed-off-by: Unicote <arvogl874@gmail.com>
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -32,6 +32,11 @@
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <android-base/properties.h>
+#include <sys/sysinfo.h>
+
+#include "property_service.h"
+#include "vendor_init.h"
 
 using android::base::GetProperty;
 
@@ -73,6 +78,29 @@ void set_ro_product_prop(const std::string &prop, const std::string &value) {
     }
 };
 
+void load_dalvik_properties() {
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+    if (sys.totalram >= 5ull * 1024 * 1024 * 1024){
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        property_override("dalvik.vm.heapstartsize", "16m");
+        property_override("dalvik.vm.heapgrowthlimit", "256m");
+        property_override("dalvik.vm.heapsize", "512m");
+        property_override("dalvik.vm.heaptargetutilization", "0.5");
+        property_override("dalvik.vm.heapmaxfree", "32m");
+    } else if (sys.totalram >= 7ull * 1024 * 1024 * 1024) {
+        // 8GB
+        property_override("dalvik.vm.heapstartsize", "24m");
+        property_override("dalvik.vm.heapgrowthlimit", "256m");
+        property_override("dalvik.vm.heapsize", "512m");
+        property_override("dalvik.vm.heaptargetutilization", "0.46");
+        property_override("dalvik.vm.heapmaxfree", "48m");
+    }
+
+    property_override("dalvik.vm.heapminfree", "8m");
+}
+
 void vendor_load_properties() {
     std::string hwname = GetProperty("ro.boot.hwname", "");
 
@@ -102,6 +130,8 @@ void vendor_load_properties() {
     if (mod_device != "") {
         property_override("ro.product.mod_device", mod_device.c_str());
     }
+
+    load_dalvik_properties();
 
     //Safetynet workarounds
     property_override("ro.oem_unlock_supported", "0");
