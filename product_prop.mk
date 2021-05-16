4ee5d35966779844aa5ca281c5aa1b4af163d069
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-05-16 11:20:51 +0000

    surya: enable dynamic refresh rate
    * SQUASHED: commits take from https://github.com/Wave-Project/device_xiaomi_surya
    
    Signed-off-by: Hazama25 <hazamafawkes@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -56,3 +56,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.enable_hwc_vds=1 \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0
+
+# Dynamic RR
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.set_idle_timer_ms=100 \
+    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true
+
